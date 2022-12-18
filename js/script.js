c681e3dec25d14518e3573d8cc343acfdebeff34
Urszula Malinowska <ula.malin35@gmail.com>
2022-12-18 17:11:47 +0100

    added function onChangeBackgroundClick

--- js/script.js
+++ js/script.js
@@ -1,49 +1,53 @@
 {
+    const songTitle = document.querySelector(".table__songTitle");
+
+    onChangeBackgroundClick = () => {
+        const body = document.querySelector(".body");
+        const buttonName = document.querySelector(".section__buttonName");
+
+        body.classList.toggle("blue");
+
+        buttonName.innerText = body.classList.contains("blue")
+            ? "jasny" :
+            "niebieski";
+
+        if (body.classList.contains("blue")) {
+            songTitle.innerText = "Afterski";
+        } else {
+            songTitle.innerText = "Mythos";
+        }
+    }
     const button = document.querySelector(".section__button");
-const body = document.querySelector(".body");
-const buttonName = document.querySelector(".section__buttonName");
-const songTitle = document.querySelector(".table__songTitle");
+
+    button.addEventListener("click", onChangeBackgroundClick);
 
 
-button.addEventListener("click", () => {
-    body.classList.toggle("blue");
 
-    buttonName.innerText = body.classList.contains("blue")
-        ? "jasny" :
-        "niebieski";
 
-    if (body.classList.contains("blue")) {
-        songTitle.innerText = "Afterski";
-    } else {
-        songTitle.innerText = "Mythos";
-    }
-});
+    onChangeFavoriteSongClick = () => {
+        const song1 = "Vi kan kjøpe hele Sverige"
+        const song2 = "Mythos"
+        const song3 = "Afterski"
 
-const song1 = "Vi kan kjøpe hele Sverige"
-const song2 = "Mythos"
-const song3 = "Afterski"
+        const favoriteSong = document.querySelector(".section__favoriteSong");
 
-const favoriteSong = document.querySelector(".section__favoriteSong");
+        favoriteSong.innerText;
+        switch (songTitle.innerText) {
+            case "Vi kan kjøpe hele Sverige":
+                return favoriteSong.innerText = song1;
 
-const songButton = document.querySelector(".section__songButton");
+            case "Mythos":
+                return favoriteSong.innerText = song2;
 
-onChangeFavoriteSongClick = () => {
-    
-    favoriteSong.innerText;
-    switch (songTitle.innerText) {
-        case "Vi kan kjøpe hele Sverige":
-           return favoriteSong.innerText = song1;
-            
-        case "Mythos":
-          return  favoriteSong.innerText = song2;
-            
-        default:
-           return favoriteSong.innerText = song3;
-           
-    }
-};
+            default:
+                return favoriteSong.innerText = song3;
 
-songButton.addEventListener("click", onChangeFavoriteSongClick);
+        }
+    };
+    const songButton = document.querySelector(".section__songButton");
+
+
+    songButton.addEventListener("click", onChangeFavoriteSongClick);
 
 
 }
